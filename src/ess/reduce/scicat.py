ffa86edfd8491b7eaeaa312e51ddd5c30ff2495a
Johannes Kasimir <johanneskasimir@gmail.com>
2024-04-08 16:43:31 +0200

    use scitacean testing fixtures, separate client provider

--- src/ess/reduce/scicat.py
+++ src/ess/reduce/scicat.py
@@ -1,29 +1,46 @@
 from pathlib import Path
-from typing import Optional
+from typing import NewType, Optional
 
 from scitacean import Client
 from scitacean.transfer.sftp import SFTPFileTransfer
 
 from .nexus import FilePath
 
+ScitaceanToken = NewType('ScitaceanToken', str)
+'''Token used to authenticate the scitacean client'''
+ScitaceanVersion = NewType('ScitaceanVersion', str)
+'''Version of the scitacean api'''
+ScitaceanClient = NewType('ScitaceanClient', Client)
+'''An instance of scitacean.Client that is used to fetch data from Scicat'''
+
+
+def sftp_ess_file_transfer() -> SFTPFileTransfer:
+    return SFTPFileTransfer(host="login.esss.dk")
+
+
+def create_scitacean_client(
+    token: ScitaceanToken,
+    file_transfer: SFTPFileTransfer,
+    version: Optional[ScitaceanVersion] = None,
+) -> ScitaceanClient:
+    if version is None:
+        version = 'v3'
+    return Client.from_token(
+        url=f"https://scicat.ess.eu/api/{version}",
+        token=token,
+        file_transfer=file_transfer,
+    )
+
 
 def download_scicat_file(
     dataset_id: str,
     filename: str,
     *,
-    token: str,
-    version: Optional[str] = None,
+    client: Client,
     target: Optional[Path] = None,
 ) -> FilePath:
-    if version is None:
-        version = 'v3'
     if target is None:
         target = Path(f'~/.cache/essreduce/{dataset_id}')
-    client = Client.from_token(
-        url=f"https://scicat.ess.eu/api/{version}",
-        token=token,
-        file_transfer=SFTPFileTransfer(host="login.esss.dk"),
-    )
     dset = client.get_dataset(dataset_id)
     dset = client.download_files(dset, target=target, select=filename)
     for f in dset.files:
--- tests/scicat_test.py
+++ tests/scicat_test.py
@@ -1,28 +1,20 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-from scitacean.testing.docs import setup_fake_client
+from scitacean.testing.backend.fixtures import (  # noqa: F401
+    client,
+    require_scicat_backend,
+)
 
-import ess
 from ess.reduce.scicat import download_scicat_file
 
 
-class Client:
-    @staticmethod
-    def from_token(url, token, file_transfer):
-        return setup_fake_client()
-
-
-ess.reduce.scicat.Client = Client
-
-
-def test_download_scicat_file():
+def test_download_scicat_file(require_scicat_backend, client):  # noqa: F811
     with TemporaryDirectory() as dname:
         path = download_scicat_file(
             '20.500.12269/72fe3ff6-105b-4c7f-b9d0-073b67c90ec3',
             'flux.dat',
-            token='123',
-            version='v3',
+            client=client,
             target=dname,
         )
         assert path == Path(dname) / 'flux.dat'
