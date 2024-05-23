2566e70d149ee7a1e9716c65fc9b1d082db83101
Neil Vaytet <neil.vaytet@ess.eu>
2024-05-23 13:43:30 +0200

    small refactor and remove erros when saving masks

--- src/ess/reduce/masking.py
+++ src/ess/reduce/masking.py
@@ -1,12 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 
+import os
 import plopp as pp
 import scipp as sc
 
-from plopp.widgets.drawing import DrawingTool
-from functools import partial
-from mpltoolbox import Hspans, Rectangles, Vspans
+from mpltoolbox import Hspans, Vspans
 
 
 def _define_rect_mask(da, rect_info):
@@ -79,25 +78,6 @@ def _get_hspan_info(artist, figure):
     }
 
 
-RectangleTool = partial(
-    DrawingTool, tool=Rectangles, get_artist_info=_get_rect_info, icon="vector-square"
-)
-
-VspansTool = partial(
-    DrawingTool,
-    tool=Vspans,
-    get_artist_info=_get_vspan_info,
-    icon="grip-lines-vertical",
-)
-
-HspansTool = partial(
-    DrawingTool,
-    tool=Hspans,
-    get_artist_info=_get_hspan_info,
-    icon="grip-lines",
-)
-
-
 def _apply_masks(da, *masks):
     out = da.copy(deep=False)
     for i, mask in enumerate(masks):
@@ -107,6 +87,10 @@ def _apply_masks(da, *masks):
 
 class MaskingTool:
     def __init__(self, data):
+        import ipywidgets as ipw
+        from plopp.widgets import DrawingTool, style
+        from mpltoolbox import Rectangles
+
         # Convert potential bin edge coords to midpoints
         da = data.copy(deep=False)
         for dim, coord in data.coords.items():
@@ -117,21 +101,33 @@ class MaskingTool:
         self.masking_node = pp.Node(_apply_masks, self.data_node)
         self.fig = pp.imagefigure(self.masking_node, norm="log")
 
-        self.controls = [
-            Tool(
-                figure=self.fig,
-                input_node=self.data_node,
-                func=func,
-                destination=self.masking_node,
-            )
-            for Tool, func in [
-                (RectangleTool, _define_rect_mask),
-                (VspansTool, _define_span_mask),
-                (HspansTool, _define_span_mask),
-            ]
-        ]
-
-        import ipywidgets as ipw
+        common = {
+            "figure": self.fig,
+            "input_node": self.data_node,
+            "destination": self.masking_node,
+        }
+        rects = DrawingTool(
+            tool=Rectangles,
+            get_artist_info=_get_rect_info,
+            icon="vector-square",
+            func=_define_rect_mask,
+            **common,
+        )
+        vspans = DrawingTool(
+            tool=Vspans,
+            get_artist_info=_get_vspan_info,
+            icon="grip-lines-vertical",
+            func=_define_span_mask,
+            **common,
+        )
+        hspans = DrawingTool(
+            tool=Hspans,
+            get_artist_info=_get_hspan_info,
+            icon="grip-lines",
+            func=_define_span_mask,
+            **common,
+        )
+        self.controls = [rects, vspans, hspans]
 
         self.fig.top_bar.add(ipw.HBox([], layout={"width": "120px"}))
         for c in self.controls:
@@ -144,9 +140,11 @@ class MaskingTool:
             c.observe(self.toggle_button_states, names="value")
 
         self.filename = ipw.Text(placeholder="Save to file")
+        self.filename.observe(self.validate_filename, names="value")
         self.save_button = ipw.Button(
-            icon="save", tooltip="Save masks", layout={"width": "40px"}
+            icon="save", tooltip="Save masks", disabled=True, **style.BUTTON_LAYOUT
         )
+        self.save_button.on_click(self.save_masks)
         self.fig.top_bar.add(self.filename)
         self.fig.top_bar.add(self.save_button)
 
@@ -156,7 +154,16 @@ class MaskingTool:
                 if c.value and c is not change["owner"]:
                     c.value = False
 
-    def save_masks(self):
+    def validate_filename(self, change):
+        path = change["new"]
+        if os.path.exists(path):
+            self.filename.style.background = "#ff9999"
+            self.save_button.disabled = True
+        else:
+            self.filename.style.background = "#99ff99"
+            self.save_button.disabled = False
+
+    def save_masks(self, _=None):
         masks = {}
         mask_counter = 0
         for c in self.controls:
@@ -164,13 +171,18 @@ class MaskingTool:
                 info = node()
                 mask_dims = "".join([axis["dim"] for axis in info.values()])
                 mask_name = f"{mask_dims}_{mask_counter}"
-                masks[mask_name] = {
-                    axis["dim"]: (axis["lower"], axis["upper"])
-                    for axis in info.values()
-                }
+                masks[mask_name] = sc.DataGroup(
+                    {
+                        axis["dim"]: sc.concat(
+                            [axis["lower"], axis["upper"]], dim=axis["dim"]
+                        )
+                        for axis in info.values()
+                    }
+                )
                 mask_counter += 1
         out = sc.DataGroup(masks)
-        out.save_hdf5()
+        out.save_hdf5(self.filename.value)
+        return out
 
 
 def masking_tool(da):
